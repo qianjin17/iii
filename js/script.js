79987274c6fb0d574397c7db7973a8ebc37e0c93
qianjin <jqian@eoas.ubc.ca>
2017-10-22 16:48:51 -0700

    huhuh

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 // part 1
-
+var APIURL = 'http://webdxd-student-api.herokuapp.com'
 $.ajax({
 	method: 'GET',
-	url:'http://webdxd-student-api.herokuapp.com/student/',
+	url: APIURL + '/student',
 })
 .done(function( studentList ) {
 	
 	for (var i = 0; i < studentList.length;i++) {
-		$('#sList').append($('<li class="showSkills" id=" ' + studentList[i]._id +  ' ">').html('<p>' + studentList[i].firstName + '</p>'))
+		$('#sList').append($('<li class="showSkills" id="' + studentList[i]._id +  '">').html('<p>' + studentList[i].firstName + '</p>'))
 	}
 })
 
 $('#sList').on('click', '.showSkills', function() {
-	var sid = ($(this).attr('id'))
-	#.ajax({
+	var sid = $(this).attr('id')
+	debugger
+	$.ajax({
 		method: 'GET',
-		url:'http://webdxd-student-api.herokuapp.com/student/'+ sid
+		url: APIURL + '/student/'+ sid
 
 	})
-	.done(function(studentDetail) {
-		var showSkills = studentDetail.skills
-		for (var i = 0; i < studentList.length;i++) {
+	.done(function( studentDetail) {
+		var skillList = studentDetail.skills
+		for (var i = 0; i < skillList.length;i++) {
 			$('#'+ studentDetail._id).append($('<span>').addClass('tag').text(skillList[i]))
 		
 		}
+		$('#'+ studentDetail._id).removeClass('showSkills')
 		
-
 	})
-
-}
+})
